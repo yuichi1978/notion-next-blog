123934d3651db416c20db199a80a6fe35254b74a
YuichiOta <rb19780519@gmail.com>
2023-08-10 13:38:52 +0900

    all done Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Breadcrumb from "./Breadcrumb";
 const Navbar = () => {
   return (
     <nav className="relative w-full flex flex-wrap items-center justify-between py-3  text-gray-500 hover:text-gray-700 focus:text-gray-700 navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-4">
         <div
           className="flex justify-between items-center bg-grey-light rounded-md w-full"
           aria-label="breadcrumb"
